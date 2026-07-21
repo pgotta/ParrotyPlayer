c144c9dd3aa14663fcae21068f0e62561361cb8c
pgotta <168211683+pgotta@users.noreply.github.com>
2026-07-21 17:58:30 -0400

    Bump app version for persistence fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.parroty.player"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     buildTypes {
